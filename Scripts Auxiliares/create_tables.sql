8bef8df15dedebf0771a82670429838fdbba341d
gabsw <sw.gabrielasantos@gmail.com>
2019-11-21 09:32:23 +0000

    Changed model data tables

--- Scripts Auxiliares/create_tables.sql
+++ Scripts Auxiliares/create_tables.sql
@@ -13,6 +13,12 @@ GO
 
 CREATE TABLE campus_monitoring.[LOCAL](
 	name		    		VARCHAR(100) 		NOT NULL,
+	max_temp_limit			NUMERIC (4, 2) 		NOT NULL,
+	min_temp_limit			NUMERIC (4, 2) 		NOT NULL,
+	max_hum_limit			NUMERIC (4, 1) 		NOT NULL    CHECK (max_hum_limit >= 0.0),
+	min_hum_limit			NUMERIC (4, 1) 		NOT NULL    CHECK (min_hum_limit >= 0.0),
+	max_co2_limit		       	NUMERIC (5, 1) 		    	    CHECK (max_co2_limit >= 0.0),
+	min_co2_limit		       	NUMERIC (5, 1) 		    	    CHECK (min_co2_limit >= 0.0),
 	PRIMARY KEY(name)
 );
 GO
