39ae9295d897b61c0b909204f737a14b78aeca31
gabsw <sw.gabrielasantos@gmail.com>
2019-11-23 21:50:15 +0000

    Adds backend infrastructure with docker-compose.

---
+++ docker/README.md
@@ -0,0 +1,35 @@
+# Instructions
+
+## Running
+
+To run, use `docker-compose up --build`. The build will force building the images.
+
+## Connecting to the database
+
+### OPTION 1
+
+`psql postgres://ies_grupo33:db_grupo33@172.22.0.2:5432/db_campus_monitoring`
+
+You can find the correct IP address by running `docker inspect ies_postgres`. The IP address can be found
+under "IPAddress". It's usually near the end.
+
+### OPTION 2
+
+Run `docker exec -it ies_postgres bash`.
+
+Then, run `psql -d db_campus_monitoring -U ies_grupo33 -W`.
+
+You'll be prompted for a password. After that, you'll be connected to the database.
+
+## Updating Postgresql Init Files
+
+If you want to update the init sql files and you have already executed `docker-compose up --build`,
+then you must delete the postgresql volume that holds the pgdata.
+
+To do that:
+1. Destroy the containers with `docker-compose rm`
+2. Destroy the `docker_postgres_data` volume with `docker volume rm docker_postgres_data`
+
+After that, you can simply run `docker-compose up` again.
+
+Note that erasing the volume will remove all data.
--- docker/postgresql/sql/01_create_tables.sql
+++ docker/postgresql/sql/01_create_tables.sql
@@ -1,42 +1,32 @@
-CREATE DATABASE campus_monitoring;
-GO
-
-USE campus_monitoring;
-GO
-
 CREATE SCHEMA campus_monitoring;
-GO
 
 -----------------------------------------------------------
 --------------------------TABLES---------------------------
 -----------------------------------------------------------
 
-CREATE TABLE campus_monitoring.[LOCAL](
+CREATE TABLE campus_monitoring.LOCAL(
 	name		    		VARCHAR(100) 		NOT NULL,
 	max_temp_limit			NUMERIC (4, 2) 		NOT NULL,
 	min_temp_limit			NUMERIC (4, 2) 		NOT NULL,
 	max_hum_limit			NUMERIC (4, 1) 		NOT NULL    CHECK (max_hum_limit >= 0.0),
 	min_hum_limit			NUMERIC (4, 1) 		NOT NULL    CHECK (min_hum_limit >= 0.0),
-	max_co2_limit		       	NUMERIC (5, 1) 		    	    CHECK (max_co2_limit >= 0.0),
+	max_co2_limit		    NUMERIC (5, 1) 		    	    CHECK (max_co2_limit >= 0.0),
 	PRIMARY KEY(name)
 );
-GO
 
 CREATE TABLE campus_monitoring.SENSOR(
-	id		        	SERIAL,
+	id		        SERIAL,
 	local_name    	VARCHAR(100) 		NOT NULL,
-	PRIMARY KEY(id),
-	FOREIGN KEY (local_name) REFERENCES campus_monitoring.[LOCAL](name)
+	PRIMARY KEY (id),
+	FOREIGN KEY (local_name) REFERENCES campus_monitoring.LOCAL(name)
 );
-GO
 
 CREATE TABLE campus_monitoring.WEATHER_READING(
-	temperature			NUMERIC (4, 2) 	NOT NULL,
-	humidity			NUMERIC (4, 1) 	NOT NULL    CHECK (humidity >= 0.0),
-	co2		        	NUMERIC (5, 1) 		    CHECK (co2 >= 0.0),
+	temperature		NUMERIC (4, 2) 	NOT NULL,
+	humidity		NUMERIC (4, 1) 	NOT NULL    CHECK (humidity >= 0.0),
+	co2		        NUMERIC (5, 1) 		        CHECK (co2 >= 0.0),
 	date_time     	TIMESTAMP     	NOT NULL,
 	sensor_id     	INT           	NOT NULL,
-	PRIMARY KEY(date_time, sensor_id),
+	PRIMARY KEY (date_time, sensor_id),
 	FOREIGN KEY (sensor_id) REFERENCES campus_monitoring.SENSOR(id)
 );
-GO
