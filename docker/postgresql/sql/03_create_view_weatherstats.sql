a4f47549c9b6656c1d14dfc3a03f89cae1ee493a
gabsw <sw.gabrielasantos@gmail.com>
2019-11-24 15:01:17 +0000

    Changed view_weather_stats.sql

--- docker/postgresql/sql/03_create_view_weatherstats.sql
+++ docker/postgresql/sql/03_create_view_weatherstats.sql
@@ -1,19 +1,19 @@
-CREATE VIEW weather_stats_view AS
-SELECT DATE(date_time),
-MAX(temperature) AS tempMax,
-MIN(temperature) AS tempMin,
-ROUND(AVG(temperature), 2) AS tempAvg,
-MAX(humidity) AS humMax,
-MIN(humidity) AS humMax,
-ROUND(AVG(humidity), 2) AS humAvg,
-MAX(co2) AS co2Max,
-MIN(co2) AS co2Min,
-ROUND(AVG(co2), 2) AS co2Avg,
-name AS localName
+CREATE VIEW campus_monitoring.weather_stats_view AS
+SELECT DATE(date_time) as "date",
+name AS local_name,
+MAX(temperature) AS temp_max,
+MIN(temperature) AS temp_min,
+ROUND(AVG(temperature), 2) AS temp_avg,
+MAX(humidity) AS hum_max,
+MIN(humidity) AS hum_min,
+ROUND(AVG(humidity), 2) AS hum_avg,
+MAX(co2) AS co2_max,
+MIN(co2) AS co2_min,
+ROUND(AVG(co2), 2) AS co2_avg
 FROM campus_monitoring.weather_reading AS wr
 JOIN campus_monitoring.sensor AS se
 ON wr.sensor_id = se.id
 JOIN campus_monitoring.local AS lo
-ON se.local_name = lo.name;
-
+ON se.local_name = lo.name
+GROUP BY DATE(date_time), name;
 
