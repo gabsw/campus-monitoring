1f588535829033073aa2e13a4f70d2cd5a90b5a1
gabsw <sw.gabrielasantos@gmail.com>
2019-11-24 10:33:00 +0000

    WeatherStats POJO, jdk changed to 8

--- Campus-Monitoring/src/main/java/ies/grupo33/CampusMonitoring/Stats/WeatherStats.java
+++ Campus-Monitoring/src/main/java/ies/grupo33/CampusMonitoring/Stats/WeatherStats.java
@@ -1,13 +1,10 @@
 package ies.grupo33.CampusMonitoring.Stats;
 
-import org.springframework.context.annotation.ComponentScan;
+import java.time.LocalDateTime;
 
-@ComponentScan
 public class WeatherStats {
 	
-	private String day;
-	private String month;
-	private String year;
+	private LocalDateTime date;
 	private double tempMax;
 	private double tempMin;
 	private double tempAvg;
@@ -17,82 +14,111 @@ public class WeatherStats {
 	private double co2Max;
 	private double co2Min;
 	private double co2Avg;
+	private String localName;
 	
 	public WeatherStats() {
 		
 	};
-	
-	public String getDay() {
-		return day;
+
+	public WeatherStats(LocalDateTime date, double tempMax, double tempMin, double tempAvg, double humMax, double humMin, double humAvg, double co2Max, double co2Min, double co2Avg, String localName) {
+		this.date = date;
+		this.tempMax = tempMax;
+		this.tempMin = tempMin;
+		this.tempAvg = tempAvg;
+		this.humMax = humMax;
+		this.humMin = humMin;
+		this.humAvg = humAvg;
+		this.co2Max = co2Max;
+		this.co2Min = co2Min;
+		this.co2Avg = co2Avg;
+		this.localName = localName;
 	}
-	public String getMonth() {
-		return month;
+
+	public LocalDateTime getDate() {
+		return date;
 	}
-	public String getYear() {
-		return year;
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
 	}
+
 	public double getTempMax() {
 		return tempMax;
 	}
-	public double getTempMin() {
-		return tempMin;
-	}
-	public double getTempAvg() {
-		return tempAvg;
-	}
-	public double getHumMax() {
-		return humMax;
-	}
-	public double getHumMin() {
-		return humMin;
-	}
-	public double getHumAvg() {
-		return humAvg;
-	}
-	public double getCo2Max() {
-		return co2Max;
-	}
-	public double getCo2Min() {
-		return co2Min;
-	}
-	public double getCo2Avg() {
-		return co2Avg;
-	}
-	public void setDay(String day) {
-		this.day = day;
-	}
-	public void setMonth(String month) {
-		this.month = month;
-	}
-	public void setYear(String year) {
-		this.year = year;
-	}
+
 	public void setTempMax(double tempMax) {
 		this.tempMax = tempMax;
 	}
+
+	public double getTempMin() {
+		return tempMin;
+	}
+
 	public void setTempMin(double tempMin) {
 		this.tempMin = tempMin;
 	}
+
+	public double getTempAvg() {
+		return tempAvg;
+	}
+
 	public void setTempAvg(double tempAvg) {
 		this.tempAvg = tempAvg;
 	}
+
+	public double getHumMax() {
+		return humMax;
+	}
+
 	public void setHumMax(double humMax) {
 		this.humMax = humMax;
 	}
+
+	public double getHumMin() {
+		return humMin;
+	}
+
 	public void setHumMin(double humMin) {
 		this.humMin = humMin;
 	}
+
+	public double getHumAvg() {
+		return humAvg;
+	}
+
 	public void setHumAvg(double humAvg) {
 		this.humAvg = humAvg;
 	}
+
+	public double getCo2Max() {
+		return co2Max;
+	}
+
 	public void setCo2Max(double co2Max) {
 		this.co2Max = co2Max;
 	}
+
+	public double getCo2Min() {
+		return co2Min;
+	}
+
 	public void setCo2Min(double co2Min) {
 		this.co2Min = co2Min;
 	}
+
+	public double getCo2Avg() {
+		return co2Avg;
+	}
+
 	public void setCo2Avg(double co2Avg) {
 		this.co2Avg = co2Avg;
 	}
 
+	public String getLocalName() {
+		return localName;
+	}
+
+	public void setLocalName(String localName) {
+		this.localName = localName;
+	}
 }
