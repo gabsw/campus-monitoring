42c21337f0c987361fc359dfa6689f5bbbf8feb5
gabsw <sw.gabrielasantos@gmail.com>
2019-11-29 18:32:52 +0000

    Added end_date_time column for UniversalAlarms

--- Scripts Auxiliares/sql-alarm/04_create_tables.sql
+++ Scripts Auxiliares/sql-alarm/04_create_tables.sql
@@ -3,14 +3,15 @@
 -----------------------------------------------------------
 
 CREATE TABLE campus_monitoring.UNIVERSAL_ALARM(
-	date_time     		TIMESTAMP     	NOT NULL,
+	start_date_time     	TIMESTAMP     	NOT NULL,
+	end_date_time     	TIMESTAMP,
 	sensor_id     		INT           	NOT NULL,
 	violation_type		VARCHAR(50)	NOT NULL,
 	violation_parameter	VARCHAR(50)	NOT NULL,
 	violation_value		NUMERIC (5, 1)	NOT NULL,
-	status				BOOLEAN			NOT NULL,
-	PRIMARY KEY (date_time, sensor_id, violation_parameter),
-	FOREIGN KEY (date_time, sensor_id) REFERENCES campus_monitoring.WEATHER_READING(date_time, sensor_id)
+	status			BOOLEAN			NOT NULL,
+	PRIMARY KEY (start_date_time, sensor_id, violation_parameter),
+	FOREIGN KEY (start_date_time, sensor_id) REFERENCES campus_monitoring.WEATHER_READING(date_time, sensor_id)
 );
 
 
