aa32fba38bc9cd717403da0fb768649521ce048c
gabsw <sw.gabrielasantos@gmail.com>
2019-12-04 22:58:53 +0000

    Adds temperature and co2 corrective factors to bme680 python script, along with proper rabbitmq connection.

--- Scripts Auxiliares/sensor.py
+++ Scripts Auxiliares/sensor.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import bme680
 import time
 import pika
 import json
 from datetime import datetime
+from subprocess import PIPE, Popen
 
 
-def get_rabbit_conn_and_channel(host, queue='task_queue'):
-    connection_ = pika.BlockingConnection(
-        pika.ConnectionParameters(host=host))
+def get_rabbit_conn_and_channel(host, username, password, queue='task_queue'):
+    credentials = pika.PlainCredentials(username, password)
+    parameters = pika.ConnectionParameters(host, 5672, '/', credentials)
+
+    connection_ = pika.BlockingConnection(parameters)
     channel_ = connection_.channel()
 
     channel_.queue_declare(queue=queue, durable=True)
@@ -21,10 +24,17 @@ def close_rabbit_connection(connection_):
 
 
 def send_message(channel_, message, queue='task_queue'):
-    channel_.queue_declare(queue='task_queue')
+    #channel_.queue_declare(queue='task_queue')
 
     channel_.basic_publish(exchange='', routing_key=queue, body=json.dumps(message),
                            properties=pika.BasicProperties(delivery_mode=2))  # make message persistent
+    print(message)
+
+
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+    output, _error = process.communicate()
+    return float(output[output.index(b'=') + 1:output.rindex(b"'")])
 
 
 try:
@@ -49,56 +59,53 @@ sensor.select_gas_heater_profile(0)
 # with their own temperature and duration.
 
 # This will be the real sensor
-sensor_id = 1
-# Fake data that is 25% higher
-factor_sensor_2 = 1.25
-
-# Fake data that is 25% lower
-factor_sensor_3 = 0.75
+sensor_id = 4
 
 # Rabbit domain will need to be changed after setup
-rabbit_domain = "localhost"
-connection, channel = get_rabbit_conn_and_channel(rabbit_domain)
+rabbit_domain = "deti-engsoft-12.ua.pt"
+#rabbit_domain = "192.168.1.216"
+connection, channel = get_rabbit_conn_and_channel(rabbit_domain, "ies_grupo33", "rabbitmq_grupo33")
+
+factor = 2.0  # Smaller numbers adjust temp down, vice versa
+smooth_size = 10  # Dampens jitter due to rapid CPU temp changes
+cpu_temps = [0]
 
 try:
     while True:
-        if sensor.get_sensor_data():
+        try:
+            if sensor.get_sensor_data():
 
-            temperature = sensor.data.temperature
-            humidity = sensor.data.humidity
-            date = datetime.now()
-            co2 = None
+                temperature = sensor.data.temperature
+                humidity = sensor.data.humidity
+                date = datetime.now()
+                co2 = None
 
-            if sensor.data.heat_stable:
-                co2 = sensor.data.gas_resistance
+                if sensor.data.heat_stable:
+                    co2 = sensor.data.gas_resistance / 250
 
-            weather_reading_1 = {"Temperature": temperature,
-                                 "Humidity": humidity,
-                                 "CO2": co2,
-                                 "Date": str(date),
-                                 "Sensor_id": sensor_id}
+                cpu_temp = get_cpu_temperature()
+                cpu_temps.append(cpu_temp)
+                if len(cpu_temps) > smooth_size:
+                    cpu_temps = cpu_temps[1:]
 
-            weather_reading_2 = {"Temperature": temperature * factor_sensor_2,
-                                 "Humidity": humidity * factor_sensor_2,
-                                 "CO2": co2 * factor_sensor_2,
-                                 "Date": str(date),
-                                 "Sensor_id": 2}
+                smoothed_cpu_temp = sum(cpu_temps) / float(len(cpu_temps))
+                temperature = temperature - ((smoothed_cpu_temp - temperature) / factor)
 
-            weather_reading_3 = {"Temperature": temperature * factor_sensor_3,
-                                 "Humidity": humidity * factor_sensor_3,
-                                 "CO2": co2 * factor_sensor_3,
-                                 "Date": str(date),
-                                 "Sensor_id": 3}
+                weather_reading = {"Temperature": temperature,
+                                   "Humidity": humidity,
+                                   "CO2": co2,
+                                   "Date": str(date),
+                                   "Sensor_id": sensor_id}
 
-            if not connection.is_open:
-                connection, channel = get_rabbit_conn_and_channel(rabbit_domain)
+                if connection is None or not connection.is_open:
+                    connection, channel = get_rabbit_conn_and_channel(rabbit_domain)
 
-            send_message(channel, weather_reading_1)
-            send_message(channel, weather_reading_2)
-            send_message(channel, weather_reading_3)
+                send_message(channel, weather_reading)
 
-            # Offset between data
-            time.sleep(1)
-
+                # Offset between data
+                time.sleep(1)
+        except Exception as ex:
+            print(ex)
+            time.sleep(300)
 except KeyboardInterrupt:
     pass
