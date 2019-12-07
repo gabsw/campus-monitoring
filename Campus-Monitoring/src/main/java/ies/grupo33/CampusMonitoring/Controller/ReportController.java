f2612d93f6a05efab55c82bf998073d8abf9e540
gabsw <sw.gabrielasantos@gmail.com>
2019-12-07 12:55:07 +0000

    Fix missing files

--- Campus-Monitoring/src/main/java/ies/grupo33/CampusMonitoring/Controller/ReportController.java
+++ Campus-Monitoring/src/main/java/ies/grupo33/CampusMonitoring/Controller/ReportController.java
@@ -1,4 +1,28 @@
 package ies.grupo33.CampusMonitoring.Controller;
 
+
+import ies.grupo33.CampusMonitoring.DTO.ReportDTO;
+import ies.grupo33.CampusMonitoring.Services.ReportServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("report")
 public class ReportController {
+    @Autowired
+    private ReportServices reportServices;
+
+    @GetMapping("/local-name/{localName}")
+    public ReportDTO getReport(@PathVariable String localName,
+                               @RequestParam(name = "start_date")
+                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                       LocalDate startDate,
+                               @RequestParam(name = "end_date")
+                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+                                       LocalDate endDate) {
+        return reportServices.buildReport(localName, startDate, endDate);
+    }
 }
--- Campus-Monitoring/src/main/java/ies/grupo33/CampusMonitoring/Services/ReportServices.java
+++ Campus-Monitoring/src/main/java/ies/grupo33/CampusMonitoring/Services/ReportServices.java
@@ -1,4 +1,62 @@
 package ies.grupo33.CampusMonitoring.Services;
 
+import ies.grupo33.CampusMonitoring.DTO.ReportDTO;
+import ies.grupo33.CampusMonitoring.Repository.ReportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+@Service
 public class ReportServices {
+    @Autowired
+    private ReportRepository reportRepository;
+
+    public ReportDTO buildReport(String localName, LocalDate startDate, LocalDate endDate) {
+        ReportDTO report = new ReportDTO(localName, startDate, endDate);
+        List<Violation> violations = generateViolations(report);
+
+        for (Violation violation : violations) {
+            int count = reportRepository.alarmCount(localName,
+                    violation.type,
+                    violation.parameter,
+                    startDate,
+                    endDate);
+            violation.setter.accept(count);
+        }
+
+        report.setAverageRating(reportRepository.averageRating(localName, startDate, endDate));
+        report.setAverageAlarmsPerDay(reportRepository.averageAlarmsPerDay(localName, startDate, endDate));
+        report.setNumberOngoingAlarms(reportRepository.countOngoingAlarms(localName, startDate, endDate));
+
+        return report;
+    }
+
+    private List<Violation> generateViolations(ReportDTO reportDTO) {
+        List<Violation> violations = new ArrayList<>();
+        violations.add(new Violation("max", "temperatura", reportDTO::setTotalMaxTempAlarms));
+        violations.add(new Violation("min", "temperatura", reportDTO::setTotalMinTempAlarms));
+        violations.add(new Violation("max", "humidade", reportDTO::setTotalMaxHumAlarms));
+        violations.add(new Violation("min", "humidade", reportDTO::setTotalMinHumAlarms));
+        violations.add(new Violation("max", "co2", reportDTO::setTotalMaxCo2Alarms));
+        return violations;
+    }
+
+
+    private class Violation {
+        String type;
+        String parameter;
+        Consumer<Integer> setter;
+
+        Violation(String type, String parameter, Consumer<Integer> setter) {
+            this.type = type;
+            this.parameter = parameter;
+            this.setter = setter;
+        }
+    }
 }
+
