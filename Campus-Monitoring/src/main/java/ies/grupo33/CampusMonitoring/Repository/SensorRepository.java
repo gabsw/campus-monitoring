0221bdd6229d6102cf1fae4410c33975e6ef350c
Alexandre Lopes <alexandrejflopes@ua.pt>
2019-12-18 22:17:31 +0000

    delete

--- Campus-Monitoring/src/main/java/ies/grupo33/CampusMonitoring/Repository/SensorRepository.java
+++
@@ -1,9 +0,0 @@
-package ies.grupo33.CampusMonitoring.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ies.grupo33.CampusMonitoring.Model.Sensor;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SensorRepository extends JpaRepository<Sensor, Long>{
-}
