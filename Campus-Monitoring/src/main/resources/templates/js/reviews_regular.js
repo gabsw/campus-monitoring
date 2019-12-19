fc43fb1d463d4bbad22241bf7800c6491ca55091
Alexandre Lopes <alxlopes@live.com.pt>
2019-12-19 17:01:06 +0000

    Update reviews_regular.js

--- Campus-Monitoring/src/main/resources/templates/js/reviews_regular.js
+++ Campus-Monitoring/src/main/resources/templates/js/reviews_regular.js
@@ -164,7 +164,7 @@ function getUserReviewsByLocal(localname) {
             }
 
             var linha = `<li class="left clearfix"><span class="chat-img pull-left">
-								<img src="../pictures/avatar_60.png" alt="User Avatar" class="img-circle" />
+								<img src="pictures/avatar_60.png" alt="User Avatar" class="img-circle" />
 								</span>
                             <div class="chat-body clearfix">     
                                 <div class="pull-left">
